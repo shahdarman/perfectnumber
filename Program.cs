d4f39f004ca7fa041a8e638f018a7675a185d85b
shahdarman <121302502+shahdarman@users.noreply.github.com>
2022-12-23 20:17:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class perfectnumber
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        
+        int Begning, End;
+
+
+        Console.WriteLine("Hello Sweety");
+
+        Console.WriteLine("------------------------------------------------------");
+
+        Console.WriteLine("Please enter The Begning : ");
+
+        Begning = int.Parse(Console.ReadLine());
+       
+        Console.WriteLine("please enter the End: ");
+        
+        End = int.Parse(Console.ReadLine());
+       
+        Console.WriteLine("The Perfect numbers within the given range : ");
+       
+        for (n = Begning; n <= End; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.WriteLine("{0} ", n);
+        }
+        Console.WriteLine("  ");
+
+        Console.WriteLine("Thank You , Have A Nice Day");
+    }
+}
